784ce659e6dbef29b7d4b04d4ffb231a5db292d4
140453a <34776660+140453a@users.noreply.github.com>
2018-11-27 09:11:04 -0400

    beautification, added timeout
    also made the -1x work by making the client use a while(true) instead of a for loop. added comments at the top, need to comment the code next and make it work with a server

--- Client.java
+++ Client.java
@@ -1,56 +1,73 @@
+// Name: Client.java
+// Authors: Joshua Alexander, Joel Deighton
+// Date: November 26, 2018
+// Purpose: Basic UDP client with socket. This is modified to send one message
+//          to a UDP server, and recieve and parse what returns.
+
+
 import java.io.*;
 import java.net.*;
 import java.util.Arrays;
 
 public class Client
 {
-   static final int MESSAGES = 10000;
-   public static void main(String args[]) throws Exception
-   {
-      BufferedReader inFromUser =
-         new BufferedReader(new InputStreamReader(System.in));
-      
-      
-      DatagramSocket clientSocket = new DatagramSocket();
-      
-      InetAddress IPAddress = InetAddress.getByName("localhost");
-      
-      byte[] sendData = new byte[30];
-      byte[] receiveData = new byte[30];
-      long[] order = new long[10000];
-      String sentence = inFromUser.readLine();
-      
-      sendData = sentence.getBytes();
-      
-      DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
-      clientSocket.send(sendPacket);
-
-      DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+    static final int MESSAGES = 10000;
+    public static void main(String args[]) throws Exception
+    {
+        BufferedReader inFromUser =
+        new BufferedReader(new InputStreamReader(System.in));
 
 
-      for(int i = 0; i < MESSAGES; i++) {
-    	  clientSocket.receive(receivePacket);
-      
-    	  String modifiedSentence = new String(receivePacket.getData());
-    	  //order[i] = Long.parseLong(modifiedSentence.trim());
-    	  String[] split = modifiedSentence.split("x");
-        int parsed = Integer.parseInt(split[0]);
+        DatagramSocket clientSocket = new DatagramSocket();
 
-        if(parsed == -1) // check for "finished" message.
+        InetAddress IPAddress = InetAddress.getByName("localhost");
+
+        byte[] sendData = new byte[30];
+        byte[] receiveData = new byte[30];
+        long[] order = new long[10000];
+        String sentence = inFromUser.readLine();
+
+        sendData = sentence.getBytes();
+        clientSocket.setSoTimeout(60000);
+
+        DatagramPacket sendPacket = 
+        new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
+
+        clientSocket.send(sendPacket);
+
+        DatagramPacket receivePacket =
+         new DatagramPacket(receiveData, receiveData.length);
+        int index = 0; 
+        try
         {
-          continue;
-        }
 
-    	  order[i] = parsed;
-    	  System.out.println(order[i]);
-    		  
-    	  System.out.println("FROM SERVER:" + modifiedSentence);
+            while(true) 
+            {
+                clientSocket.receive(receivePacket);
 
-      }
-      System.out.println("Final results: " + Arrays.toString(order));
-      clientSocket.close(); 
-   }
+                String modifiedSentence = new String(receivePacket.getData());
+    	           //order[i] = Long.parseLong(modifiedSentence.trim());
+                String[] split = modifiedSentence.split("x");
+                int parsed = Integer.parseInt(split[0]);
+
+                if(parsed == -1) // check for "finished" message.
+                {
+                    System.out.println("Transmission complete.");
+                    break;
+                }
+
+                order[index] = parsed;
+                System.out.println(order[index]);
+
+                System.out.println("FROM SERVER:" + modifiedSentence);
+                index = index + 1;
+            }
+        } catch (SocketException e)
+          {
+              System.err.println("Timed out after 60 seconds...
+                Probably your first message got lost: " + e);    
+          }
+        System.out.println("Final results: " + Arrays.toString(order));
+        clientSocket.close(); 
+    }
 }
-
-// NOTE: You should just keep server and client barebones, and move all the testing to a new test-class. Keep the x0000 padding in server,
-// but move other things to new class :)
--- Server.java
+++ Server.java
@@ -1,61 +1,67 @@
+// Name: Server.java
+// Authors: Joshua Alexander, Joel Deighton
+// Date: November 26, 2018
+// Purpose: Basic UDP server with socket. When it receives a message, it then
+//          sends a stream of packets over a period of time to the receiving
+//          ip address. The form of the packet is (index)x0000... with the
+//          resulting packet being 30 bytes long.
+
 import java.net.*;
 import java.util.Random;
 
 
-public class Server
-{
+public class Server {
     static final int MESSAGES = 10000;
     static final int BYTE_SIZE = 30;
     static final int PORT_NUMBER = 9876;
-   public static void main(String args[]) throws Exception
-      {
-         DatagramSocket serverSocket = new DatagramSocket(PORT_NUMBER);
-            byte[] receiveData = new byte[BYTE_SIZE];
-            byte[] sendData = new byte[BYTE_SIZE];
-            int count = 1;
-            Random rand = new Random();
-            
-            while(true)
-               {
-                  DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-                  serverSocket.receive(receivePacket);
-                  
-                  String sentence = new String( receivePacket.getData());
-                  System.out.println("RECEIVED: " + sentence);
-                  
-                  InetAddress IPAddress = receivePacket.getAddress();
-                  
-                  int port = receivePacket.getPort();
-                  
-                  //String capitalizedSentence = sentence.toUpperCase();
-                  //sendData = capitalizedSentence.getBytes();
-                  for(int i = 0; i < MESSAGES; i++)
-                  {
-                	  String fill = Integer.toString(count) + "x";
-                	  for(int j = BYTE_SIZE; j > String.valueOf(fill).length(); j --)
-                	  {
-                		  fill = fill + "0";
-                	  }
-                	  sendData =  fill.getBytes();
-                	  DatagramPacket sendPacket =
-                			  new DatagramPacket(sendData, sendData.length, IPAddress, port);
-                  
-                	  Thread.sleep(2);
-                	  serverSocket.send(sendPacket);
-                	  count = count + 1;
-                  }
-                  Thread.sleep(6000) // Wait one minute for all packets to come in
-                  finishedMsg = "-1x";
-                  finishedData = finishedMsg.getBytes();
-                  for(int i = 0; i < 30; i++)
-                  {
-                      DatagramPacket finishedPacket =
-                              new DatagramPacket(finishedData, finishedData.length, IPAddress, port); 
-                        Thread.sleep(1000); // sleep 1 second, sending 30 messages over 30 seconds
-                        serverSocket.send(finishedPacket);                   
-                  }
-                  serverSocket.close();  
-                  System.exit(0);                  
-               }             
-      }
+    public static void main(String args[]) throws Exception {
+        DatagramSocket serverSocket = new DatagramSocket(PORT_NUMBER);
+        byte[] receiveData = new byte[BYTE_SIZE];
+        byte[] sendData = new byte[BYTE_SIZE];
+        byte[] finishedData = new byte[BYTE_SIZE];
+        int count = 1;
+        Random rand = new Random();
+
+        while (true)
+        {
+            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+            serverSocket.receive(receivePacket);
+
+            String sentence = new String(receivePacket.getData());
+            System.out.println("RECEIVED: " + sentence);   
+
+            InetAddress IPAddress = receivePacket.getAddress();
+
+            int port = receivePacket.getPort();
+
+
+            for (int i = 0; i < MESSAGES; i++)
+            {
+                String fill = Integer.toString(count) + "x";
+                for (int j = BYTE_SIZE; j > String.valueOf(fill).length(); j--)
+                {
+                    fill = fill + "0";
+                }
+                sendData = fill.getBytes();
+                DatagramPacket sendPacket =
+                new DatagramPacket(sendData, sendData.length, IPAddress, port);
+
+                Thread.sleep(2);
+                serverSocket.send(sendPacket);
+                count = count + 1;
+            }
+            Thread.sleep(60000); // Wait one minute for all packets to come in
+            String finishedMsg = "-1x";
+            finishedData = finishedMsg.getBytes();
+            for (int i = 0; i < 30; i++) 
+            {
+                DatagramPacket finishedPacket =
+                new DatagramPacket(finishedData, finishedData.length, IPAddress, port);
+                Thread.sleep(1000); // sleep 1 second, sending 30 messages over 30 seconds
+                serverSocket.send(finishedPacket);
+            }
+            serverSocket.close();
+            System.exit(0);
+        }
+    }
 }
